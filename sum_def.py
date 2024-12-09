2545b7ba77e04140181a9a364de7b5e3ba323814
Stanislav <boar_100@mail.ru>
2024-12-09 18:18:18 +0500

    Def sum numbers

---
+++ sum_def.py
@@ -0,0 +1,9 @@
+def sum(a, b, c, d):
+    return (a + b + c + d)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+c = int(input('Enter 3st number: '))
+d = int(input('Enter 4nd number: '))
+
+print(f'Sum of {a} and {b} and {c} and {d} is {sum(a, b, c, d)}')
